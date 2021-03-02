cf55ecc252bda5fba41758a5bb406e0b871a8e53
Rhalll <78082777+Rhalll@users.noreply.github.com>
2021-03-02 10:45:32 +0100

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -68,11 +68,11 @@ echo.
 goto error
 
 :OkJHome
-if exist "%JAVA_HOME%\bin\java.exe" goto init
+if exist "C:\Program Files\Java\jdk-15.0.2\bin\java.exe" goto init
 
 echo.
 echo Error: JAVA_HOME is set to an invalid directory. >&2
-echo JAVA_HOME = "%JAVA_HOME%" >&2
+echo JAVA_HOME = "C:\Program Files\Java\jdk-15.0.2" >&2
 echo Please set the JAVA_HOME variable in your environment to match the >&2
 echo location of your Java installation. >&2
 echo.
@@ -116,7 +116,7 @@ for /F "usebackq delims=" %%a in ("%MAVEN_PROJECTBASEDIR%\.mvn\jvm.config") do s
 
 :endReadAdditionalConfig
 
-SET MAVEN_JAVA_EXE="%JAVA_HOME%\bin\java.exe"
+SET MAVEN_JAVA_EXE="C:\Program Files\Java\jdk-15.0.2\bin\java.exe"
 set WRAPPER_JAR="%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar"
 set WRAPPER_LAUNCHER=org.apache.maven.wrapper.MavenWrapperMain
 
